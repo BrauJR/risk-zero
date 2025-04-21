4415fe9bd1e2ff83af416887309e75988dbe1a25
BrauJR <romejor3030@gmail.com>
2025-04-21 12:54:36 -0600

    Blog

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
-// next.config.js
-
+// next.config.ts
 import withMDX from '@next/mdx'
 
 const nextConfig = {
   experimental: {
-    appDir: true, // Si estás usando el directorio `app` de Next.js
+    appDir: true, // Solo habilita esto si estás usando `appDir` en Next.js 13
   },
   pageExtensions: ['ts', 'tsx', 'mdx'], // Permite .mdx como extensión de página
 }
 
 export default withMDX({
-  ...nextConfig, // Mantenemos la configuración de Next.js
-  extension: /\.mdx$/, // Especificamos que .mdx debe ser procesado
+  ...nextConfig,
+  extension: /\.mdx$/, // Permite .mdx como extensión para las páginas
 })
